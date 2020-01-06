f6f6a792be7bbf5e05d90e06f6fb8f2fae7816ef
Nelio Alves <acenelio@gmail.com>
2020-01-06 19:06:57 -0300

    Fix baskara.c

--- c/baskara.c
+++ c/baskara.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main()
+{
     double a, b, c, delta, x1, x2;
 
-	printf("Coeficiente a: ");
+    printf("Coeficiente a: ");
     scanf("%lf", &a);
 
-	printf("Coeficiente b: ");
+    printf("Coeficiente b: ");
     scanf("%lf", &b);
 
-	printf("Coeficiente c: ");
+    printf("Coeficiente c: ");
     scanf("%lf", &c);
 
     delta = (b * b) - (4 * a * c);
 
-    if (delta < 0) {
+    if (a == 0 || delta < 0)
+    {
         printf("Esta equacao nao possui raizes reais\n");
     }
-    else {
+    else
+    {
         x1 = ((-b) + sqrt(delta)) / (2 * a);
         x2 = ((-b) - sqrt(delta)) / (2 * a);
 
-		printf("X1 = %.4lf\n", x1);
-		printf("X2 = %.4lf\n", x2);
+        printf("X1 = %.4lf\n", x1);
+        printf("X2 = %.4lf\n", x2);
     }
 
-	return 0;
+    return 0;
 }
